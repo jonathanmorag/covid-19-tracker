8d46cc87caddadcfe4b9901b41dc6c32277afb3a
Jonathan Morag <moragjonathan@gmail.com>
2020-06-22 15:44:49 +0300

    Init repo

---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import { fetchCountries } from "../../api";
+
+import styles from "./Country.module.css";
+
+const Country = ({ handleCountryChange }) => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, [setFetchedCountries]);
+
+  console.log(fetchedCountries);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect
+        defaultValue=""
+        onChange={(e) => handleCountryChange(e.target.value)}
+      >
+        <option value="">Global</option>
+        {fetchedCountries.map((country, key) => (
+          <option key={key} value={country}>
+            {country}
+          </option>
+        ))}
+      </NativeSelect>
+    </FormControl>
+  );
+};
+
+export default Country;
