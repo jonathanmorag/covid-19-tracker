499819c7317518a47f613dbbc928e57b0fa01e70
Jonathan Morag <moragjonathan@gmail.com>
2020-06-22 15:54:30 +0300

    Updates

---
+++ precache-manifest.39551ed6dcb56f1bd684fd33f2f9a892.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a167d36746b57d9fb7118948a0d79156",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "d8e0da24cc75ae7e1d89",
+    "url": "/covid-19-tracker/static/css/main.404192ae.chunk.css"
+  },
+  {
+    "revision": "b3572546680d74beefd5",
+    "url": "/covid-19-tracker/static/js/2.22ac3459.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.22ac3459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8e0da24cc75ae7e1d89",
+    "url": "/covid-19-tracker/static/js/main.67add35e.chunk.js"
+  },
+  {
+    "revision": "969486af163cf7dd1ee6",
+    "url": "/covid-19-tracker/static/js/runtime-main.9f16eefb.js"
+  },
+  {
+    "revision": "d8125466a421b6bb8087a84fef92b3c9",
+    "url": "/covid-19-tracker/static/media/covid.d8125466.png"
+  }
+]);
